02ad8caed24ffc5d6bc052556a2b7909c6673dc9
Ayush Kumar Jha <ayushkj21@gmail.com>
2026-09-14 18:56:11 +0530

    add+overload method chaining

--- lab12/prg1.cpp
+++ lab12/prg1.cpp
@@ -6,7 +6,7 @@ class point {
     public :
     point(int x=0 , int y=0):x{x},y{y}{}
 
-    friend void operator<<(ostream &os , point p);
+friend ostream &operator<<(ostream &os , point p);
 
     point operator+(point p){
         int a = x + p.x ;
@@ -15,16 +15,15 @@ class point {
         return q;
     }
 };
-     void operator<<(ostream &os , point p){
-        cout<<p.x<<" "<<p.y<<endl;
- }
 
+ostream &operator<<(ostream &os , point p){
+    os<<p.x<<" "<<p.y<<endl;
+    return os; 
+}
 
 int main(){
     point p1(10,5);
     point p2(-9,16);
     point p3 = p1 + p2 ;
-    cout<<p1;
-    cout<<p2;
-    cout<<p3;
+    cout<<p1<<p2<<p3; // method chaining 
 }
