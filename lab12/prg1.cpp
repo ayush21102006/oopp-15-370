4b370c526d99bed21b22fafd663a3a1fbd556b79
Ayush Kumar Jha <ayushkj21@gmail.com>
2026-09-14 18:51:53 +0530

    add+overload no chaining

--- lab12/prg1.cpp
+++ lab12/prg1.cpp
@@ -5,10 +5,8 @@ class point {
     int x , y ;
     public :
     point(int x=0 , int y=0):x{x},y{y}{}
-    
-    void show(){
-        cout<<x<<" , "<<y<<endl;
-    }
+
+    friend void operator<<(ostream &os , point p);
 
     point operator+(point p){
         int a = x + p.x ;
@@ -17,17 +15,16 @@ class point {
         return q;
     }
 };
-
-
-
+     void operator<<(ostream &os , point p){
+        cout<<p.x<<" "<<p.y<<endl;
+ }
 
 
 int main(){
     point p1(10,5);
     point p2(-9,16);
     point p3 = p1 + p2 ;
-    p1.show();
-    p2.show();
-    p3.show();
-
+    cout<<p1;
+    cout<<p2;
+    cout<<p3;
 }
