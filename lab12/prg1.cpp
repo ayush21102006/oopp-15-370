5444f3f703749a0f1c6d66e1edb28e9d9098ed7b
Ayush Kumar Jha <ayushkj21@gmail.com>
2026-09-14 18:40:53 +0530

    add+overload

---
+++ lab12/prg1.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+class point {
+    int x , y ;
+    public :
+    point(int x=0 , int y=0):x{x},y{y}{}
+    
+    void show(){
+        cout<<x<<" , "<<y<<endl;
+    }
+
+    point operator+(point p){
+        int a = x + p.x ;
+        int b = y + p.y ;
+        point q(a,b);
+        return q;
+    }
+};
+
+
+
+
+
+int main(){
+    point p1(10,5);
+    point p2(-9,16);
+    point p3 = p1 + p2 ;
+    p1.show();
+    p2.show();
+    p3.show();
+
+}
