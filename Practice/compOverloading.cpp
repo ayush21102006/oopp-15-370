75c845a17191efc34ef6c5607fc706902feae99f
Ayush Kumar Jha <ayushkj21@gmail.com>
2026-09-17 09:41:47 +0530

    using int

--- Practice/compOverloading.cpp
+++ Practice/compOverloading.cpp
@@ -20,24 +20,24 @@ class comp{
     //     return comp(real + c.real , img + c.img);
     // }
 
-    friend comp operator +(comp c , comp d);
+    friend comp operator +(comp c , int x);
 
     void show(){
         cout<<real<<" , "<<img<<endl;
     }
 };
 
-comp operator +(comp c , comp d){
-    return comp(c.real + d.real , c.img + d.img);
+comp operator +(comp c , int x){
+    return comp(c.real + x , c.img + x);
 }
 
 
 int main(){
     comp c1(5,20);
-    comp c2(10,15);
+    comp c2;
     c1.show();
-    c2.show();
-    comp c3 = c1 + c2;
+    // c2.show();
+    comp c3 = c1 + 5;
     c3.show();
 
 return 0;
