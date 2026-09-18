caaba29e3f8f7e07724a13df0ad501f22f8f59f3
Ayush Kumar Jha <ayushkj21@gmail.com>
2026-09-18 11:49:35 +0530

    Overloading int + comp

--- Practice/compOverloading.cpp
+++ Practice/compOverloading.cpp
@@ -20,16 +20,27 @@ class comp{
     //     return comp(real + c.real , img + c.img);
     // }
 
-    friend comp operator +(comp c , int x);
+    /*friend comp operator +(comp c , int x);*/
+
+    friend comp operator +(int x , comp c);
+
 
     void show(){
         cout<<real<<" , "<<img<<endl;
     }
 };
 
-comp operator +(comp c , int x){
+/*comp operator +(comp c , int x){
     return comp(c.real + x , c.img + x);
+}*/
+
+comp operator +(int x , comp c){
+    return comp(x + c.real ,x + c.img);
 }
+
+
+/* When left side of any binary operator is universal(int , long , float , char ), any predefined data , 
+then that operator must be overloaded as friend function .*/
 
 
 int main(){
@@ -37,7 +48,8 @@ int main(){
     comp c2;
     c1.show();
     // c2.show();
-    comp c3 = c1 + 5;
+    /*comp c3 = c1 + 5;*/
+    comp c3 = 6 + c1;
     c3.show();
 
 return 0;
